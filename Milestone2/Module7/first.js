43c78553d55130dabd0f7c3d1ec94e5bc384e3b3
Asif Hosen <asifhosen105010@gmail.com>
2026-07-23 22:18:06 +0600

    milestone 1 and 2 added

---
+++ Milestone2/Module7/first.js
@@ -0,0 +1,18 @@
+console.log(10);
+console.log(12);
+console.log(3344);
+
+let name = "Asif";
+console.log(typeof name);
+
+
+let numberString = '20';
+let number = parseInt(numberString);
+console.log(typeof number, number);
+
+
+let a = 0.1;
+let b = 0.2;
+let total = a + b;
+console.log(total);
+console.log(total.toFixed(2));
